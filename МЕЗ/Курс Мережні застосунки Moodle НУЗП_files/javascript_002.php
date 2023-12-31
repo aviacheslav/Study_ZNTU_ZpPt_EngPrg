a365594d76cb81d0a8609f4918f0d1648e3364dc
Viacheslav <afonin.viacheslav@yandex.ru>
2023-12-31 16:44:32 +0300

    first commit before New Year

---
+++ МЕЗ/Курс Мережні застосунки Moodle НУЗП_files/javascript_002.php
@@ -0,0 +1,3 @@
+function togglecollapseall(iscoursecontext){if($('dl').hasClass('expanded')){$('.toggle').removeClass('open');if(!iscoursecontext){$('dd').addClass('block_grade_me_hide')}
+$('dd ul').addClass('block_grade_me_hide');$('dl').removeClass('expanded')}else{$('.toggle').addClass('open');if(!iscoursecontext){$('dd').removeClass('block_grade_me_hide')}
+$('dd ul').removeClass('block_grade_me_hide');$('dl').addClass('expanded')}}
